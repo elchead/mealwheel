4d8a9b06222c5b9100385302b611c6fcf91dab33
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-03-04 13:21:58 +0100

    put App into App folder

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./App/App";
 import reportWebVitals from "./reportWebVitals";
-import Album from "./Album";
+import Album from "./App/Album";
 
 ReactDOM.render(
   <React.StrictMode>
