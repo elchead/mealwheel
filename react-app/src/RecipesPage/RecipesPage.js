5352b895f78ac6db407ddce5cdbb61dbea975be5
Adrian Stobbe <a@s.com>
2021-03-20 13:45:13 +0100

    add link to recipe page

--- react-app/src/RecipesPage/RecipesPage.js
+++ react-app/src/RecipesPage/RecipesPage.js
@@ -4,20 +4,20 @@ import Header from "./Header";
 import Content from "./Content";
 
 function RecipesPage() {
-    return (
-        <Grid container direction="column">
-            <Grid item>
-                <Header/> 
-            </Grid>
-            <Grid item container>
-                <Grid item xs={false} sm={2}/>
-                <Grid item xs={12} sm={8}>
-                    <Content/>
-                </Grid>
-                <Grid item xs={false} sm={2}/>
-             </Grid>
+  return (
+    <Grid container direction="column">
+      <Grid item>
+        <Header />
+      </Grid>
+      <Grid item container>
+        <Grid item xs={false} sm={2} />
+        <Grid item xs={12} sm={8}>
+          <Content />
         </Grid>
-    );  
+        <Grid item xs={false} sm={2} />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default RecipesPage;
