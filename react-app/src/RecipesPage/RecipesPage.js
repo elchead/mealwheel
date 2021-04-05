c1c42d9767f969235a025f43698e6bf9b2137ad0
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-04-05 16:45:27 +0200

    fix layout for recipe page (same as homepage)

--- react-app/src/RecipesPage/RecipesPage.js
+++ react-app/src/RecipesPage/RecipesPage.js
@@ -7,8 +7,43 @@ import Recipe from "../components/Recipes/Recipe";
 import { useSelector } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import { userService } from "../_services";
-const useStyles = makeStyles({});
-
+// const useStyles = makeStyles({});
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minWidth: 275,
+  },
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: "#fff",
+    padding: theme.spacing(8, 0, 6),
+    marginRight: 100,
+    marginLeft: 100,
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+}));
 function RecipesPage() {
   const userId = useSelector((state) =>
     state.authentication.user ? state.authentication.user.id : undefined
